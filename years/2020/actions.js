01f35d1bf1caa29c3ad0ae97d7a2637ec61a12c0
iboyperson <tjquillan@gmail.com>
2020-03-04 23:26:02 -0700

    hopefully remove NaN

--- years/2020/actions.js
+++ years/2020/actions.js
@@ -1,12 +1,22 @@
 var actions = {
   moveLineFrequency: (array) => {
     var freq = (array.filter((value) => value === "Yes").length / array.length) * 100
-    return freq !== NaN ? freq : 0
+
+    if (freq === NaN) {
+      freq = 0
+    }
+
+    return freq
   },
   accuracy: (array) => {
     var array = array.map((array) => array.reduce((prev, curr) => prev + curr, 0))
     var ans = (array[0] / array.reduce((prev, curr) => prev + curr, 0)) * 100
-    return ans !== NaN ? ans : 0
+
+    if (ans === NaN) {
+      ans = 0
+    }
+
+    return ans
   },
   accuracyStr: (array) => {
     actions.accuracy(array) + "%"

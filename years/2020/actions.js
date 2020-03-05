fdfb49d8b6421b3c1d18b1123d595e5b7b9778ba
iboyperson <tjquillan@gmail.com>
2020-03-04 23:35:22 -0700

    change to !isNan

--- years/2020/actions.js
+++ years/2020/actions.js
@@ -2,13 +2,13 @@ var actions = {
   moveLineFrequency: (array) => {
     var freq = (array.filter((value) => value === "Yes").length / array.length) * 100
 
-    return Number.isNaN(freq) ? freq : 0
+    return !Number.isNaN(freq) ? freq : 0
   },
   accuracy: (array) => {
     var array = array.map((array) => array.reduce((prev, curr) => prev + curr, 0))
     var ans = (array[0] / array.reduce((prev, curr) => prev + curr, 0)) * 100
 
-    return Number.isNaN(ans) ? ans : 0
+    return !Number.isNaN(ans) ? ans : 0
   },
   accuracyStr: (array) => {
     actions.accuracy(array) + "%"

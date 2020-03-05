def1e47fcda683a43909e43a147a1e363fad09f0
iboyperson <tjquillan@gmail.com>
2020-03-04 23:34:11 -0700

    use isNaN

--- years/2020/actions.js
+++ years/2020/actions.js
@@ -2,21 +2,13 @@ var actions = {
   moveLineFrequency: (array) => {
     var freq = (array.filter((value) => value === "Yes").length / array.length) * 100
 
-    if (freq === NaN) {
-      freq = 0
-    }
-
-    return freq
+    return Number.isNaN(freq) ? freq : 0
   },
   accuracy: (array) => {
     var array = array.map((array) => array.reduce((prev, curr) => prev + curr, 0))
     var ans = (array[0] / array.reduce((prev, curr) => prev + curr, 0)) * 100
 
-    if (ans === NaN) {
-      ans = 0
-    }
-
-    return ans
+    return Number.isNaN(ans) ? ans : 0
   },
   accuracyStr: (array) => {
     actions.accuracy(array) + "%"

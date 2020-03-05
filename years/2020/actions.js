a39f745ac5d54d09b35769d0bfb8f0b320be0bcb
iboyperson <tjquillan@gmail.com>
2020-03-04 22:07:07 -0700

    Switch to 2020

---
+++ years/2020/actions.js
@@ -0,0 +1,10 @@
+var actions = {
+  moveLineFrequency: (array) => {
+    var freq = array.filter((value) => value === "Yes").length / array.length
+    return freq + "%"
+  },
+  accuracy: (array) => {
+    var length = array[0].length + array[1].length
+    return (array[0].length / length) + "%"
+  }
+}

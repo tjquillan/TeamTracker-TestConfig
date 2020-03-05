946ec648516ea7d59060ed1393024658283caa43
iboyperson <tjquillan@gmail.com>
2020-03-04 22:59:48 -0700

    fix

--- years/2020/actions.js
+++ years/2020/actions.js
@@ -1,11 +1,12 @@
 var actions = {
   moveLineFrequency: (array) => {
     var freq = array.filter((value) => value === "Yes").length / array.length
-    return freq
+    return freq !== NaN ? ans : 0
   },
   accuracy: (array) => {
     var length = array[0].length + array[1].length
-    return (array[0].length / length)
+    var ans = (array[0].length / length)
+    return ans !== NaN ? ans : 0
   },
   accuracyStr: (array) => {
     actions.accuracy(array) + "%"

c450e67435036e0ed19aad0d37a7eded72ca42d4
iboyperson <tjquillan@gmail.com>
2020-03-04 23:05:25 -0700

    make nums look like percents

--- years/2020/actions.js
+++ years/2020/actions.js
@@ -1,11 +1,11 @@
 var actions = {
   moveLineFrequency: (array) => {
-    var freq = array.filter((value) => value === "Yes").length / array.length
+    var freq = (array.filter((value) => value === "Yes").length / array.length) * 100
     return freq !== NaN ? freq : 0
   },
   accuracy: (array) => {
     var length = array[0].length + array[1].length
-    var ans = (array[0].length / length)
+    var ans = (array[0].length / length) * 100
     return ans !== NaN ? ans : 0
   },
   accuracyStr: (array) => {

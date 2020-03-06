62935376250d93e77271243f2ac3bfd4809d372e
iboyperson <tjquillan@gmail.com>
2020-03-05 22:21:16 -0800

    updates

--- years/2020/actions.js
+++ years/2020/actions.js
@@ -4,6 +4,9 @@ var actions = {
 
     return !Number.isNaN(freq) ? freq : 0
   },
+  moveLineFrequencyStr: () => {
+    return actions.moveLineFrequency(array) + "%"
+  },
   accuracy: (array) => {
     var array = array.map((array) => array.reduce((prev, curr) => prev + curr, 0))
     var ans = (array[0] / array.reduce((prev, curr) => prev + curr, 0)) * 100
@@ -12,5 +15,11 @@ var actions = {
   },
   accuracyStr: (array) => {
     actions.accuracy(array) + "%"
+  },
+  sum: (array) => {
+    return array.reduce((prev, curr) => prev + curr, 0)
+  },
+  averagePerMatch: (array) => {
+    return actions.sum(array) / array.length
   }
 }

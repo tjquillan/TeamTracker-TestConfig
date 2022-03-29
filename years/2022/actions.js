aaacc52b235bf40ccea8ce07bf5b3d1a95a65d4e
booprc6@gmail.com <33536239+Nomless@users.noreply.github.com>
2022-03-29 12:07:58 -0700

    Make robotShootLocations action better

--- years/2022/actions.js
+++ years/2022/actions.js
@@ -1,14 +1,12 @@
 var actions = {
   moveLineFrequency: (array) => {
     var freq = (array.filter((value) => value === "Yes").length / array.length) * 100
-
     return !Number.isNaN(freq) ? freq : 0
   },
   moveLineFrequencyStr: (array) => actions.moveLineFrequency(array) + "%",
 
   robotBreak: (array) => {
     var freq = (array.filter((value) => value === "Yes").length / array.length) * 100
-
     return !Number.isNaN(freq) ? freq : 0
   },
   robotBreakStr: (array) => actions.robotBreak(array) + "%",
@@ -22,11 +20,8 @@ var actions = {
     return !Number.isNaN(average) ? average.toFixed(2) : 0
   },
   averagePerMatchStr: (array) => actions.averagePerMatch(array),
+  
   robotShootLocations: (array) => {
-    return Array.from(
-      new Set(array.reduce((prev, curr) => {
-        return (Array.isArray(prev) ? prev : prev.split(", ")).concat(Array.isArray(curr) ? curr : curr.split(", "))
-      }, []))
-    ).join(", ")
+    return Array.from(new Set(array.map(a => a.split(", ")).flat())).join(", ")
   } 
 }

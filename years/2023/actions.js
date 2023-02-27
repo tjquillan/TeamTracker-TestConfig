ab6a3f599ba44139b28cef80e46ee6e61721d30d
booprc6@gmail.com <33536239+Nomless@users.noreply.github.com>
2023-02-27 12:00:03 -0700

    Change title

---
+++ years/2023/actions.js
@@ -0,0 +1,27 @@
+var actions = {
+  moveLineFrequency: (array) => {
+    var freq = (array.filter((value) => value === "Yes").length / array.length) * 100
+    return !Number.isNaN(freq) ? freq : 0
+  },
+  moveLineFrequencyStr: (array) => actions.moveLineFrequency(array) + "%",
+
+  robotBreak: (array) => {
+    var freq = (array.filter((value) => value === "Yes").length / array.length) * 100
+    return !Number.isNaN(freq) ? freq : 0
+  },
+  robotBreakStr: (array) => actions.robotBreak(array) + "%",
+  
+  sum: (array) => {
+    var sum = array.reduce((prev, curr) => prev + curr, 0)
+    return !Number.isNaN(sum) ? sum : 0
+  },  
+  averagePerMatch: (array) => {
+    var average = actions.sum(array) / array.length
+    return !Number.isNaN(average) ? average.toFixed(2) : 0
+  },
+  averagePerMatchStr: (array) => actions.averagePerMatch(array),
+  
+  robotShootLocations: (array) => {
+    return Array.from(new Set(array.filter(a => typeof a === "string" && a.trim().length > 0).map(a => a.split(", ")).flat())).join(", ")
+  } 
+}

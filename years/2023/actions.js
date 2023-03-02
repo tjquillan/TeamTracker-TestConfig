76852490dd8affd3b58cb2ce02d445435e02cef7
booprc6@gmail.com <33536239+Nomless@users.noreply.github.com>
2023-03-01 19:54:09 -0700

    Update to Hueneme Port Regional

--- years/2023/actions.js
+++ years/2023/actions.js
@@ -1,27 +1,28 @@
 var actions = {
-  moveLineFrequency: (array) => {
-    var freq = (array.filter((value) => value === "Yes").length / array.length) * 100
-    return !Number.isNaN(freq) ? freq : 0
-  },
-  moveLineFrequencyStr: (array) => actions.moveLineFrequency(array) + "%",
-
-  robotBreak: (array) => {
-    var freq = (array.filter((value) => value === "Yes").length / array.length) * 100
-    return !Number.isNaN(freq) ? freq : 0
-  },
-  robotBreakStr: (array) => actions.robotBreak(array) + "%",
-  
-  sum: (array) => {
-    var sum = array.reduce((prev, curr) => prev + curr, 0)
-    return !Number.isNaN(sum) ? sum : 0
-  },  
-  averagePerMatch: (array) => {
+  average: (array) => {
+    if(array === undefined) return 0
     var average = actions.sum(array) / array.length
     return !Number.isNaN(average) ? average.toFixed(2) : 0
   },
-  averagePerMatchStr: (array) => actions.averagePerMatch(array),
-  
-  robotShootLocations: (array) => {
-    return Array.from(new Set(array.filter(a => typeof a === "string" && a.trim().length > 0).map(a => a.split(", ")).flat())).join(", ")
-  } 
+
+  sum: (array) => {
+    if(array === undefined) return 0
+    var sum = array.reduce((prev, curr) => prev + curr, 0)
+    return !Number.isNaN(sum) ? sum : 0
+  },
+
+  set: (array) => {
+    if(array === undefined) return Array()
+    return Array.from(
+      new Set(
+        array.filter(
+          a => typeof a === "string" && a.trim().length > 0
+        ).map(a => a.split(", ")).flat()
+      )
+    )
+  },
+
+  setStr: (array) => {
+    return actions.set(array).join(", ")
+  }
 }
